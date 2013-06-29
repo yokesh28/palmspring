4b7e1515522d512f1cfb92669ee753a506d65c63
screative2 <screative2@ubuntu>
2013-06-29 12:32:36 +0530

    Signed-off-by: screative2 <screative2@ubuntu>

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Palm Spring</title>
+<title>Palm Spring.....</title>
 <meta charset="UTF-8">
 <link rel="stylesheet" href="css/style.css">
 <script src="js/jquery-1.10.1.min.js"></script>
