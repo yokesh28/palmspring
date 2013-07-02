e1987ab34f7d3cd8c8acee654342e51bec5f2d41
screative2 <screative2@ubuntu>
2013-07-02 17:25:56 +0530

    Signed-off-by: screative2 <screative2@ubuntu>

--- js/script.js
+++ js/script.js
@@ -14,20 +14,23 @@ function enableClick(child){
 	$('.menu_inner li:nth-child('+child+')>img').animate({"left":"-25px",opacity:1});
 	$('.menu_inner li:nth-child('+child+')>a').animate({left:"20px",opacity:1});
 	$('#box'+child+'').animate({right:"0px",opacity:1},600);
-
+	
 	setTimeout(function(){$('.menu_inner li:nth-child('+child+')>a').css('background','#619CD8');},300);
+	$('.close').animate({opacity:1},3500);
 	
 	
 }
 function closeBox()
 {
+	$('.close').animate({opacity:0});
 	$('.box').animate({right:"-720px",opacity:0},800);
+	
 	}
-function disableClick(child){
+function disableClick(){
 	
 			$('.menu_inner li img').animate({"left":"0px",opacity:0});
 			$('.menu_inner li a').animate({left:"0px"});
-			$('#box'+child+'').animate({right:"-720px",opacity:0});
+			$('.box').animate({right:"-720px",opacity:0});
 			$('.menu_inner li a').css('background','white');
 	
 
