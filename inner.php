ea0ca66707e176d7bbc7f6af48f8ed9558709833
annamalai <annamalai@annamalai-desktop>
2013-07-02 16:25:17 +0530

    Signed-off-by: annamalai <annamalai@annamalai-desktop>

--- inner.php
+++ inner.php
@@ -16,16 +16,7 @@
 		<img src="img/hp.jpg" alt="Palm Spring" class="hm_back">
 
 		<?php include 'header_inner.php';?>
-<div class="box7" style="right: 0%; opacity: 1;">
-			<h5>Overview</h5>
-			<p>When you live in an environment of peace and quietude.away
-				from the clamor.the uproar and the grime and dust of city life. it
-				sure can soothe your soul and transport you to a world your
-				own-exclusive,private and spacious. But when you have it right with
-				in city limits.its joy compounded. Padur has got it all and more.
-				sheer elegance matched by functional aesthetics that not only makes
-				you feel good but look good.</p>
-		</div>
+
 
 	</div>
 </body>
