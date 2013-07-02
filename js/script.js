de43e350eb76c076de276d763619556e340a2427
screative2 <screative2@ubuntu>
2013-07-02 17:34:52 +0530

    Signed-off-by: screative2 <screative2@ubuntu>

--- js/script.js
+++ js/script.js
@@ -13,17 +13,18 @@ function hidePreloader() {
 function enableClick(child){
 	$('.menu_inner li:nth-child('+child+')>img').animate({"left":"-25px",opacity:1});
 	$('.menu_inner li:nth-child('+child+')>a').animate({left:"20px",opacity:1});
-	$('#box'+child+'').animate({right:"0px",opacity:1},600);
+	$('#box'+child+'').animate({right:"0px",opacity:1},300);
 	
 	setTimeout(function(){$('.menu_inner li:nth-child('+child+')>a').css('background','#619CD8');},300);
-	$('.close').animate({opacity:1},3500);
+	$('.close').animate({opacity:1},2000);
 	
 	
 }
 function closeBox()
 {
 	$('.close').animate({opacity:0});
-	$('.box').animate({right:"-720px",opacity:0},800);
+	
+	$('.box').animate({right:"-720px",opacity:0},300);
 	
 	}
 function disableClick(){
