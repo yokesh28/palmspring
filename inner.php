54d796cdeca018acc358978cc399e90aa11fce2c
screative2 <screative2@ubuntu>
2013-07-04 14:48:14 +0530

    Signed-off-by: screative2 <screative2@ubuntu>

--- inner.php
+++ inner.php
@@ -54,221 +54,227 @@
 		<img src="img/close.png" alt="close" class="close">
 		<div class="box" id="box1">
 
-		
-		<h2>Overview </h2>
-		<p>Palm Springs is the latest residential spectacle created by 
-one of Coimbatore's most renowned realtors, MMRF. 
-Carefully nestled at Ottiyambakkam, Chennai; Palm Springs 
-is a magnificent cluster of 448 top-notch 1, 2, 3 BHK 
-apartments spread over a wide expanse of over 6.8 acres. 
-Every home at Palm Springs is a result of artful aesthetics, 
-plentiful amenities and flawless architecture. Ranging from 
-573 sq. ft. to 1350 sq. ft., each vasthu-complaint 
-Palm Springs home has access to the state-of-the-art 
-facilities, ensuring all comfort, convenience and luxury in 
-the world is under one single roof. What's more,
-Palm Springs truly enjoys the benefit of being located close 
-to the leading IT corridors, reputed educational institutions 
-and the biggest shopping malls. So when you buy your 
-dream-home at Palm Springs, be sure you have the best of all worlds. </p></div>
-					<div class="box" id="box2">
-		<h2>Amenities</h2>
-		<p>A host of the most modern amenities ensure your every 
-moment spent at Palm Springs is as engaging as
-entertaining. Palm Springs features a state-of-the-art 
-swimming pool, fully equipped club house, basket ball, 
-tennis courts and much more; so every member of your 
-family is devoid of boredom, in the pinkest of health and 
-in absolute peace of mind. So when you invest in 
-Palm Springs, you can be sure you would be indulging in a 
-kind of delight that will continually enthrall you for a lifetime to come. </p></div>
-					<div class="box" id="box3">
-		<h2>Location</h2>
-		<img width=" 600px;" src="gallery/loc.jpg"></div>
-					<div class="box" id="box4">
-		<h2>Specifications</h2>
-		<h4>Structure</h4>
-		<div class="sp_detail">
-		<p>
-R.C.C. Slab Framed Structure with Brick Panel walls plastered with cement 
-mortar.</p>		
+
+			<h2>Overview</h2>
+			<p>Palm Springs is the latest residential spectacle created by one of
+				Coimbatore's most renowned realtors, MMRF. Carefully nestled at
+				Ottiyambakkam, Chennai; Palm Springs is a magnificent cluster of 448
+				top-notch 1, 2, 3 BHK apartments spread over a wide expanse of over
+				6.8 acres. Every home at Palm Springs is a result of artful
+				aesthetics, plentiful amenities and flawless architecture. Ranging
+				from 573 sq. ft. to 1350 sq. ft., each vasthu-complaint Palm Springs
+				home has access to the state-of-the-art facilities, ensuring all
+				comfort, convenience and luxury in the world is under one single
+				roof. What's more, Palm Springs truly enjoys the benefit of being
+				located close to the leading IT corridors, reputed educational
+				institutions and the biggest shopping malls. So when you buy your
+				dream-home at Palm Springs, be sure you have the best of all worlds.
+			</p>
 		</div>
-		<h4>Flooring</h4>
-		<div class="sp_detail">
-<p>2'*2' Vitrified Tiles flooring in all the rooms.</p>
+		<div class="box" id="box2">
+			<h2>Amenities</h2>
+			<p>A host of the most modern amenities ensure your every moment spent
+				at Palm Springs is as engaging as entertaining. Palm Springs
+				features a state-of-the-art swimming pool, fully equipped club
+				house, basket ball, tennis courts and much more; so every member of
+				your family is devoid of boredom, in the pinkest of health and in
+				absolute peace of mind. So when you invest in Palm Springs, you can
+				be sure you would be indulging in a kind of delight that will
+				continually enthrall you for a lifetime to come.</p>
+		</div>
+		<div class="box" id="box3">
+			<h2>Location</h2>
+			<img width=" 600px;" src="gallery/loc.jpg">
+		</div>
+		<div class="box" id="box4">
+			<h2>Specifications</h2>
+			<h4>Structure</h4>
+			<div class="sp_detail">
+				<p>R.C.C. Slab Framed Structure with Brick Panel walls plastered
+					with cement mortar.</p>
+			</div>
+			<h4>Flooring</h4>
+			<div class="sp_detail">
+				<p>2'*2' Vitrified Tiles flooring in all the rooms.</p>
 
-     </div>
-     <h4>Toilets</h4>
-		<div class="sp_detail">
-<p>Ceramic Flooring (matt finish) and dado with glazed wall tiles up to 7'0”, 
-standard quality C.P. Fittings, Standard EWC / Wash basin. </p>
-     </div>
-     <h4>Doors</h4>
-		<div class="sp_detail">
-<p>Main Door with Teak frames & Panel Door with Brass fitting. Inner doors will 
-be flush doors with painted finished. Water proof flush doors for toilets. </p>
-     </div>
-     <h4>Windows</h4>
-		<div class="sp_detail">
-<p>UPVC frames with glass shutters. </p>
-     </div>
-      <h4>Kitchen</h4>
-		<div class="sp_detail">
-<p>Raised cooking platform with granite top with SS sink
-Glazed tiles will be provided for a height of 3' for the length of the cooking 
-platform. </p>
-     </div>
-      <h4> Electrical</h4>
-     <div class="sp_detail">
-<p>3 Phase supply with modular switches. 
-Electrical outlets for A/c in all bedrooms. 
-Adequate fan, light, telephone and television points. 
-Standby sound proof generator facility for the common areas and lift. </p>
-     </div>
-     
-     <h4> Painting</h4>
-     <div class="sp_detail">
-<p>Good quality Emulsion painting with putty finish for all the rooms. </p>
-     </div>
-     <h4>  Water supply</h4>
-     <div class="sp_detail">
-<p>Ground water with sufficient depth to give continuous supply of water with 
-over head tank fitted with motor & pump. A sump with motor will be 
-provided in the ground to store  </p>
-     </div>
-     <h4>  Path ways</h4>
-     <div class="sp_detail">
-<p>Inter locking Stones will be provided in common areas and masonry 
-compound walls with MS Grill Gates.  </p></div>
-<h4>  Amenities</h4>
- <div class="sp_detail">
-<p>Gardening, Club House, Swimming pool, Children Play Area, 
-Sewage Treatment Plant , Party hall & kids room, Intercom Connections and 
-Security systems.</p>
-     </div>
-     
+			</div>
+			<h4>Toilets</h4>
+			<div class="sp_detail">
+				<p>Ceramic Flooring (matt finish) and dado with glazed wall tiles up
+					to 7'0”, standard quality C.P. Fittings, Standard EWC / Wash basin.
+				</p>
+			</div>
+			<h4>Doors</h4>
+			<div class="sp_detail">
+				<p>Main Door with Teak frames & Panel Door with Brass fitting. Inner
+					doors will be flush doors with painted finished. Water proof flush
+					doors for toilets.</p>
+			</div>
+			<h4>Windows</h4>
+			<div class="sp_detail">
+				<p>UPVC frames with glass shutters.</p>
+			</div>
+			<h4>Kitchen</h4>
+			<div class="sp_detail">
+				<p>Raised cooking platform with granite top with SS sink Glazed
+					tiles will be provided for a height of 3' for the length of the
+					cooking platform.</p>
+			</div>
+			<h4>Electrical</h4>
+			<div class="sp_detail">
+				<p>3 Phase supply with modular switches. Electrical outlets for A/c
+					in all bedrooms. Adequate fan, light, telephone and television
+					points. Standby sound proof generator facility for the common areas
+					and lift.</p>
+			</div>
 
-    
-
-     
-
-    
-
-     
+			<h4>Painting</h4>
+			<div class="sp_detail">
+				<p>Good quality Emulsion painting with putty finish for all the
+					rooms.</p>
+			</div>
+			<h4>Water supply</h4>
+			<div class="sp_detail">
+				<p>Ground water with sufficient depth to give continuous supply of
+					water with over head tank fitted with motor & pump. A sump with
+					motor will be provided in the ground to store</p>
+			</div>
+			<h4>Path ways</h4>
+			<div class="sp_detail">
+				<p>Inter locking Stones will be provided in common areas and masonry
+					compound walls with MS Grill Gates.</p>
+			</div>
+			<h4>Amenities</h4>
+			<div class="sp_detail">
+				<p>Gardening, Club House, Swimming pool, Children Play Area, Sewage
+					Treatment Plant , Party hall & kids room, Intercom Connections and
+					Security systems.</p>
+			</div>
 
 
 
-     
+
+
+
+
+
+
+
+
+
+
 
 		</div>
-					<div class="box box9" id="box5">
-		<h2>Gallery</h2>
-		
+		<div class="box box9" id="box5">
+			<h2>Gallery</h2>
 
 
-			
+
+
 			<ul>
 
-				
-					<li class="i6"><a href="gallery/g1.png"
-					rel="lightbox[roadtrip]"><img src="gallery/g1.png"></a></li>
-					<li class="i7"><a href="gallery/g2.png"
-					rel="lightbox[roadtrip]"><img src="gallery/g2.png"></a></li>
-					<li class="i8"><a href="gallery/g3.png"
-					rel="lightbox[roadtrip]"><img src="gallery/g3.png"></a></li>
-					
-					
+
+				<li class="i6"><a href="images/gallery-big-1.jpg"
+					rel="lightbox[roadtrip]"><img src="gallery/g1.png"> </a></li>
+				<li class="i7"><a href="images/gallery-big-1.jpg"
+					rel="lightbox[roadtrip]"><img src="gallery/g2.png"> </a></li>
+				<li class="i8"><a href="images/gallery-big-1.jpg"
+					rel="lightbox[roadtrip]"><img src="gallery/g3.png"> </a></li>
+
+
 
 			</ul>
 
 
 
 
-		</div></div>
-					<div class="box box10" id="box6">
+		</div>
+	</div>
+	<div class="box box10" id="box6">
 		<h2>Floor plans</h2>
 		<ul>
 
-				
-					<li class="i6"><a href="gallery/f1.jpg"
-					rel="lightbox[roadtrip]"><img src="gallery/f1.jpg"></a></li>
-					<li class="i7"><a href="gallery/f2.jpg"
-					rel="lightbox[roadtrip]"><img src="gallery/f2.jpg"></a></li>
-					<li class="i8"><a href="gallery/f3.jpg"
-					rel="lightbox[roadtrip]"><img src="gallery/f3.jpg"></a></li>
-					<li class="i9"><a href="gallery/f4.jpg"
-					rel="lightbox[roadtrip]"><img src="gallery/f4.jpg"></a></li>
-					
-					
 
-			</ul></div>
-							
-							
-							
-							
-								<div class="box" id="box7">
+			<li class="i6"><a href="images/big-floor-1.jpg"
+				rel="lightbox[roadtrip]"><img src="gallery/f1.jpg"> </a></li>
+			<li class="i7"><a href="images/big-floor-2.jpg"
+				rel="lightbox[roadtrip]"><img src="gallery/f2.jpg"> </a></li>
+			<li class="i8"><a href="images/big-floor-3.jpg"
+				rel="lightbox[roadtrip]"><img src="gallery/f3.jpg"> </a></li>
+			<li class="i9"><a href="images/big-floor-4.jpg"
+				rel="lightbox[roadtrip]"><img src="gallery/f4.jpg"> </a></li>
+
+
+
+		</ul>
+	</div>
+
+
+
+
+	<div class="box" id="box7">
 		<h2>Contact</h2>
-			<div id="contactForm">
-				<img class="exit1" src="menulist/cls.png">
-				<form class="contactForm" name="cform" id="c_form"
-					onSubmit="return validateForm();">
+		<div id="contactForm">
 
-					<div class="input_boxes">
+			<form class="contactForm" name="cform" id="c_form"
+				onSubmit="return validateForm();">
 
-
+				<div class="input_boxes">
 
 
 
 
-						<div class="alert">
-							<span class="name-missing">Name-missing</span>
-						</div>
-						<div id="inp_box">
-							<label>Name:</label> <input id="name" type="text" value=""
-								name="name" onblur="clearText(this)" onfocus="clearText(this)" />
-						</div>
-						<!--inp box-->
 
 
-						<div class="alert">
-							<span class="email-missing">Email-missing</span>
-						</div>
-						<div id="inp_box">
-							<label>E-Mail:</label> <input id="e-mail" type="text" value=""
-								name="email" onblur="clearText(this)" onfocus="clearText(this)" />
-						</div>
-						<!--inp box-->
-
-						<div class="alert">
-							<span class="phone-missing">Phone-missing </span>
-						</div>
-						<div id="inp_box">
-							<label>Phone:</label> <input id="phone" type="text" value=""
-								name="phone" onblur="clearText(this)" onfocus="clearText(this)" />
-						</div>
-						<!--inp box-->
-
-						<div class="alert">
-							<span class="message-missing"></span>
-						</div>
-						<div id="msg_box">
-							<label>Message</label>
-							<textarea id="message" rows="" cols="" name="message"
-								onblur="clearText(this)" onfocus="clearText(this)"></textarea>
-						</div>
-						<!--msg box-->
-						<input class="sendmessage" type="submit" name="submit"
-							value="Send Messsage"> <span id="response"
-							style="color: black"></span>
+					<div class="alert">
+						<span class="name-missing">Name-missing</span>
 					</div>
-					<!--input boxes-->
+					<div id="inp_box">
+						<label>Name:</label> <input id="name" type="text" value=""
+							name="name" onblur="clearText(this)" onfocus="clearText(this)" />
+					</div>
+					<!--inp box-->
 
 
-				</form>
-			</div>
-			<!--contact form--></div>
+					<div class="alert">
+						<span class="email-missing">Email-missing</span>
+					</div>
+					<div id="inp_box">
+						<label>E-Mail:</label> <input id="e-mail" type="text" value=""
+							name="email" onblur="clearText(this)" onfocus="clearText(this)" />
+					</div>
+					<!--inp box-->
 
-<script type="text/javascript">
+					<div class="alert">
+						<span class="phone-missing">Phone-missing </span>
+					</div>
+					<div id="inp_box">
+						<label>Phone:</label> <input id="phone" type="text" value=""
+							name="phone" onblur="clearText(this)" onfocus="clearText(this)" />
+					</div>
+					<!--inp box-->
+
+					<div class="alert">
+						<span class="message-missing"></span>
+					</div>
+					<div id="msg_box">
+						<label>Message</label>
+						<textarea id="message" rows="" cols="" name="message"
+							onblur="clearText(this)" onfocus="clearText(this)"></textarea>
+					</div>
+					<!--msg box-->
+					<input class="sendmessage" type="submit" name="submit"
+						value="Send Messsage"> <span id="response" style="color: black"></span>
+				</div>
+				<!--input boxes-->
+
+
+			</form>
+		</div>
+		<!--contact form-->
+	</div>
+
+	<script type="text/javascript">
 function validateForm()
 {
 	
@@ -353,7 +359,7 @@ return false;
 <script type="text/javascript">
 $('.menu_inner li:nth-child(1)>img').animate({"left":"-25px",opacity:1});
 $('.menu_inner li:nth-child(1)>a').animate({left:"20px",opacity:1});
-$('#box1').animate({right:"0px",opacity:0.9},900);
+$('#box1').animate({right:"0px",opacity:1},900);
 $('.menu_inner li:nth-child(1)').animate({"margin-left":"28px"});
 $('.close').click(function(){
 	$('.close').css({'opacity':'0'});
